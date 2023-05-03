615284756f78212856eb74f04fe67fa59dd0baf7
tejas <tejasanand.iitdcse@gmail.com>
2023-05-03 15:17:48 +0530

    did problems 1 and 2

--- subseq.pl
+++ subseq.pl
@@ -1,1 +1,19 @@
-subseq(A,[]).
+sizeof([],0).
+sizeof([_X|Xs],N) :-
+    sizeof(Xs,A),
+    N is A + 1.
+% subseq([],A).
+% subseq([X|Xs],A) :- 
+% I have used 1 based indexing
+splitter(X,0,Y) :- X=Y .
+splitter([_Hd|Tl],N,Y) :-
+    A is N-1,
+    splitter(Tl,A,Y).
+mymember(X,[X|_Tl],1).
+mymember(X,[_Y|Tl],N) :- 
+    mymember(X,Tl,A),
+    N is A+1.
+% Will calculate the first index of any element X in a list
+subsequence([],_A).
+subsequence([X|Xs],[X|Ys]) :- subsequence(Xs,Ys).
+subsequence(A,[_Y|Ys]) :- subsequence(A,Ys).
