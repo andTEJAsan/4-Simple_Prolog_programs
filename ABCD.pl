6676e05428745fedf78a94eaea7ae64916263aee
tejas <tejasanand.iitdcse@gmail.com>
2023-05-05 06:27:23 +0530

    did some thing

--- ABCD.pl
+++ ABCD.pl
@@ -25,15 +25,17 @@ generator(LeftBank,RightBank,Forward,Backward,NewLeft,NewRight,NewFor,NewBack) :
 check_paddleAlice([]).
 check_paddleAlice([[X,Y]|Xs] ) :- X \=1, X \= Y, check_paddleAlice(Xs).
 check_paddleBob([]).
-check_paddleBob([[X,Y]| Xs]) :- (
-    (X=2, Y=3);
-    ,(X \=2)
-), check_paddleBob(Xs).
+check_paddleBob([[X,Y]| Xs]) :- X \= 2, check_paddleBob(Xs).
+check_paddleBob([[X,Y]| Xs]) :- X=2,Y=3, check_paddleBob(Xs).
 PaddleNot(_,[]).
 PaddleNot(A,[[X,Y]| Xs]) :- X \= A, PaddleNot(A,Xs).
 
 check_paddleAll(X) :- not(PaddleNot(1,X)),not(PaddleNot(2,X)),not(PaddleNot(3,X)),not(PaddleNot(4,X)).
-
+final(X,A,B,C,D) :-
+    generator(X,[],[],[],A,B,C,D)
+    ,check_paddleAlice(C)
+    ,check_paddleBob(C)
+    ,check_paddleAll(C).
 
 
 %check_paddle(X,Y,People_reached) :- X /= Y, X /= 1,
